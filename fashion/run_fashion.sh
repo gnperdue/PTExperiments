9b5a471c34415c8a726ca76a70223b3b892de68e
Gabriel Perdue <gnperdue@me.com>
2018-09-05 16:02:17 -0500

    runner scripts

---
+++ fashion/run_fashion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATADIR="${HOME}/Dropbox/Data/RandomData/hdf5"
+python fashion.py --data-dir $DATADIR
---
+++ fashion/run_test_fashion.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+DATADIR="${HOME}/Dropbox/Data/RandomData/hdf5"
+python test_fashion.py --data-dir $DATADIR --show

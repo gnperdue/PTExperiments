a8e55f89c5dafef392c17fb0e9c3d2f1b0165e30
Gabriel Perdue <gnperdue@me.com>
2018-11-24 06:42:26 -0600

    remove unused vars, set to conv model

--- run_scripts/wilson_cluster/pt_gridrl_exe.sh
+++ run_scripts/wilson_cluster/pt_gridrl_exe.sh
@@ -11,15 +11,11 @@ cp ${BASEPATH}/gridrl/*.py ./gridrl/
 cp ${BASEPATH}/nnqrt.py .
 
 EXE="nnqrt.py"
-NUM_EPOCHS=100
-
-MODEL_DIR="/data/perdue/minerva/tensorflow/models/test"
-DATA_DIR="/data/perdue/minerva/hdf5/201804"
-TRAIN_FILE="${DATA_DIR}/hadmultkineimgs_127x94_me1Nmc.hdf5"
-EVAL_FILE="${DATA_DIR}/hadmultkineimgs_127x94_me1Omc.hdf5"
+NUM_EPOCHS=5000
 
 ARGS="--num-epochs ${NUM_EPOCHS}"
 #ARGS+=" --batch-size ${BATCH_SIZE}"
+ARGS+=" --conv"
 
 
 SNGLRTY="/data/perdue/singularity/gnperdue-singularity_imgs-master-py3_trch041.simg"

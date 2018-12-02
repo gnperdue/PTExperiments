0cec3e8ffc66ea58231e44d65fb6f68067d3d096
Gabriel Perdue <gnperdue@me.com>
2018-12-02 07:37:25 -0600

    clarify purpose a bit

--- fnem/scratch.py
+++ fnem/scratch.py
@@ -4,8 +4,12 @@ import matplotlib.pyplot as plt
 from sim.oscillators import Oscillator
 
 osc = Oscillator(add_noise=True, drops=True)
-plt.scatter(osc.ts(), osc.dts(), c='r')
-plt.scatter(osc.ts(), osc.dTs(), c='b')
+plt.scatter(osc.ts(), osc.d1s(), c='r')
+plt.scatter(osc.ts(), osc.d2s(), c='g')
+plt.scatter(osc.ts(), osc.d3s(), c='b')
+plt.scatter(osc.ts(), osc.d4s(), c='y')
+plt.scatter(osc.ts(), osc.dts(), c='k')
+# plt.scatter(osc.ts(), osc.dTs(), c='k')
 plt.show()
 for _ in range(10):
     osc.step()
--- fnem/sim/oscillators.py
+++ fnem/sim/oscillators.py
@@ -14,28 +14,29 @@ class Oscillator(object):
         self.spacing = spacing
         self.add_noise = add_noise
         self.drops = drops
-        self.times = []
-        self.d1, self.d2, self.d3, self.d4 = [], [], [], []
-        self.dt, self.dT = [], []
+        self.times, self.d1, self.d2, self.d3, self.d4, self.dT = \
+            [], [], [], [], [], []
         for i in range(self.nsteps):
             self._add_data_point()
             self.t = self.t + self.spacing
 
     def _add_data_point(self):
         if len(self.times) >= self.nsteps:
-            for a in [self.times, self.d1, self.d2, self.d3, self.d4,
-                      self.dt, self.dT]:
+            for a in [self.times, self.d1, self.d2, self.d3, self.d4, self.dT]:
                 a.pop(0)
         self.times.append(self.t)
+        raw_vs = []
+        nos_vs = []
         for i, a in enumerate([self.d1, self.d2, self.d3, self.d4]):
-            v = 0.0
-            if not (self.drops and np.random.random() < Oscillator.drop_prob):
-                v = Oscillator.amp[i] * np.cos(Oscillator.frq[i] * self.t) \
-                    + Oscillator.nos[i] * np.random.randn() \
-                    if self.add_noise else 0.0
-            a.append(v)
-        self.dt.append(self.d1[-1] + self.d2[-1])
-        self.dT.append(self.dt[-1] + self.d3[-1] + self.d3[-1])
+            raw_vs.append(
+                Oscillator.amp[i] * np.cos(Oscillator.frq[i] * self.t)
+            )
+            nos_vs.append(Oscillator.nos[i] * np.random.randn())
+            if self.drops and np.random.random() < Oscillator.drop_prob:
+                a.append(0.0)
+            else:
+                a.append(raw_vs[-1] + nos_vs[-1] if self.add_noise else 0.0)
+        self.dT.append(sum(raw_vs))
 
     def step(self):
         self._add_data_point()
@@ -44,8 +45,21 @@ class Oscillator(object):
     def ts(self):
         return np.asarray(self.times)
 
+    def d1s(self):
+        return np.asarray(self.d1)
+
+    def d2s(self):
+        return np.asarray(self.d2)
+
+    def d3s(self):
+        return np.asarray(self.d3)
+
+    def d4s(self):
+        return np.asarray(self.d4)
+
     def dts(self):
-        return np.asarray(self.dt)
+        return np.asarray(self.d1) + np.asarray(self.d2) \
+            + np.asarray(self.d3) + np.asarray(self.d4)
 
     def dTs(self):
         return np.asarray(self.dT)

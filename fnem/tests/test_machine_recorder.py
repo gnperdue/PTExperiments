75ee849565a3d599e542dad4aef4632f9b02d575
Gabriel Perdue <gnperdue@gmail.com>
2018-12-03 10:34:44 -0600

    read and write in the test

--- fnem/tests/test_machine_recorder.py
+++ fnem/tests/test_machine_recorder.py
@@ -18,17 +18,33 @@ class TestMachineRecorder(unittest.TestCase):
     def tearDown(self):
         self.recorder.cleanup_files()
 
-    def test_write_data(self):
-        t = 1.0
-        meas = np.asarray([10.1, 1.1, 0.6, 0.2], dtype=np.float32)
-        targ = np.asarray([10.0, 1.0, 0.5, 0.1], dtype=np.float32)
-        success = self.recorder.write_data(t, meas, targ)
+    def test_writeread_data(self):
+        # write a line of dummy data
+        t0 = 1.0
+        meas0 = np.asarray([10.1, 1.1, 0.6, 0.2], dtype=np.float32)
+        targ0 = np.asarray([10.0, 1.0, 0.5, 0.1], dtype=np.float32)
+        a0 = np.concatenate(([t0], meas0, targ0))
+        success = self.recorder.write_data(t0, meas0, targ0)
         self.assertTrue(success)
-        t = 1.1
-        meas = np.asarray([10.2, 0.9, 0.4, 0.2], dtype=np.float32)
-        targ = np.asarray([10.3, 1.1, 0.3, 0.1], dtype=np.float32)
-        success = self.recorder.write_data(t, meas, targ)
+        # write another line of dummy data
+        t1 = 1.1
+        meas1 = np.asarray([10.2, 0.9, 0.4, 0.2], dtype=np.float32)
+        targ1 = np.asarray([10.3, 1.1, 0.3, 0.1], dtype=np.float32)
+        a1 = np.concatenate(([t1], meas1, targ1))
+        success = self.recorder.write_data(t1, meas1, targ1)
         self.assertTrue(success)
+        # close and zip the file
+        self.recorder.close()
+        # read the contents into an array of strings
+        content = self.recorder.read_data()
+        # parse the content
+        line0 = map(float, content[0].split(','))
+        line1 = map(float, content[1].split(','))
+        # check we read what we wrote
+        for a, b in zip(a0, line0):
+            self.assertAlmostEqual(a, b, places=6)
+        for a, b in zip(a1, line1):
+            self.assertAlmostEqual(a, b, places=6)
 
 
 if __name__ == '__main__':

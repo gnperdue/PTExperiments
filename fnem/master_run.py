5eb5cd35a08d03a9ae645d2b4cd3d8db64909760
Gabriel Perdue <gnperdue@gmail.com>
2018-12-04 14:14:03 -0600

    stub script for master runner

---
+++ fnem/master_run.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+'''
+Running modes:
+
+* run a trained policy and generate historical data
+* run a policy with live training from historical data
+* run a policy with live training using an asynchronous running simulation
+'''

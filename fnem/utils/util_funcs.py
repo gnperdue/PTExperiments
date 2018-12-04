7c95d379e5dc15e1b94b2d387e5708c173e131ec
Gabriel Perdue <gnperdue@gmail.com>
2018-12-04 14:47:12 -0600

    add some useful utils

---
+++ fnem/utils/util_funcs.py
@@ -0,0 +1,27 @@
+import logging
+
+LOGGER = logging.getLogger(__name__)
+
+
+def get_logging_level(log_level):
+    log_level = log_level.upper()
+    logging_level = logging.INFO
+    if log_level == 'DEBUG':
+        logging_level = logging.DEBUG
+    elif log_level == 'INFO':
+        logging_level = logging.INFO
+    elif log_level == 'WARNING':
+        logging_level = logging.WARNING
+    elif log_level == 'ERROR':
+        logging_level = logging.ERROR
+    elif log_level == 'CRITICAL':
+        logging_level = logging.CRITICAL
+    else:
+        print('Unknown or unset logging level. Using INFO')
+
+    return logging_level
+
+
+def count_parameters(model):
+    '''https://discuss.pytorch.org/t/how-do-i-check-the-number-of-parameters-of-a-model/4325/8'''
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)

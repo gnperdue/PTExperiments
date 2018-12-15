6afdcb9ad9347fc496f5a937d829e4d4b429e428
Gabriel Perdue <gnperdue@me.com>
2018-12-15 07:45:07 -0600

    add an end-to-end test in the unit framework

--- fnem/tests/test_machine.py
+++ fnem/tests/test_machine.py
@@ -3,26 +3,101 @@ Usage:
     python test_machine.py -v
     python test_machine.py
 '''
-import logging
 import unittest
+import time
+import os
+
 import numpy as np
+import matplotlib.pyplot as plt
+
 from sim.recorders import MachineStateTextRecorder as Recorder
 from sim.data_model import DataGenerator as Generator
 from sim.data_model import NoiseModel as Noise
+from sim.engines import SimulationMachine
+from policies.rule_based import SimpleRuleBased
+
+LOG_TEMPLATE = 'log_machinewithrule_%d'
+PLT_TEMPLATE = 'plt_machinewithrule_%d'
+REFERNECE_LOG = './reference_files/' + (LOG_TEMPLATE % 1544881179) + '.csv.gz'
+REFERENCE_PLT = './reference_files/' + (PLT_TEMPLATE % 1544881179) + '.pdf'
 
 
-class TestMachine(unittest.TestCase):
+class TestMachineWithRuleBased(unittest.TestCase):
 
     def setUp(self):
-        gen = Generator()
-        noise = Noise()
-        recorder = Recorder('./test_log')
+        np.random.seed(0)
+        self.test_time = time.time()
+        self.machine_log = './' + LOG_TEMPLATE % self.test_time
+        self.machine_plt = './' + PLT_TEMPLATE % self.test_time + '.pdf'
+
+        dgen = Generator()
+        nosgen = Noise()
+        recorder = Recorder(self.machine_log)
+        self.machine = SimulationMachine(
+            setting=10.0, data_generator=dgen, noise_model=nosgen,
+            logger=recorder
+        )
+        self.policy = SimpleRuleBased(
+            time=0.0, setting=10.0, amplitude=10.0, period=2.0,
+            commands_array=self.machine.get_commands()
+        )
 
     def tearDown(self):
         pass
 
-    def test_basic(self):
-        pass
+    def test_settings(self):
+        self.assertEqual(self.machine.get_setting(), 10.0)
+        # update machine by command, check setting
+
+    def test_end_to_end_simplerulebased_run(self):
+        m1 = []
+        m2 = []
+        m3 = []
+        m4 = []
+        totals = []
+        heat = []
+        settings = []
+        ts = []
+        for i in range(2000):
+            self.machine.step()
+            t = self.machine.get_time()
+            sensor_vals = self.machine.get_sensor_values()
+            ts.append(t)
+            for i, m in enumerate([m1, m2, m3, m4]):
+                m.append(sensor_vals[i])
+            totals.append(sum(sensor_vals))
+            settings.append(self.machine.get_setting())
+            heat.append(self.machine.get_heat())
+            state = sensor_vals + [t]
+            self.policy.set_state(state)
+            command = self.policy.compute_action()
+            self.machine.update_machine(command)
+            self.policy.update_setting(command)
+
+        self.machine.close_logger()
+        reference_log_size = os.stat(REFERNECE_LOG).st_size
+        new_log_size = os.stat(self.machine_log + '.csv.gz').st_size
+        self.assertEqual(reference_log_size, new_log_size)
+
+        fig = plt.Figure(figsize=(10, 6))
+        gs = plt.GridSpec(1, 4)
+        ax1 = plt.subplot(gs[0])
+        ax1.scatter(ts, m1, c='r')
+        ax1.scatter(ts, m2, c='g')
+        ax1.scatter(ts, m3, c='b')
+        ax1.scatter(ts, m4, c='y')
+        ax2 = plt.subplot(gs[1])
+        ax2.scatter(ts, totals, c='k')
+        ax3 = plt.subplot(gs[2])
+        ax3.scatter(ts, heat, c='k')
+        ax4 = plt.subplot(gs[3])
+        ax4.scatter(ts, settings, c='k')
+
+        fig.tight_layout()
+        plt.savefig(self.machine_plt, bbox_inches='tight')
+        reference_plot_size = os.stat(REFERENCE_PLT).st_size
+        new_plot_size = os.stat(self.machine_plt).st_size
+        self.assertEqual(reference_plot_size, new_plot_size)
 
 
 if __name__ == '__main__':

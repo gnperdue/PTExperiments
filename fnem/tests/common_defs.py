9c69dfec254429d1e12f8b7b458942013f102b05
Gabriel Perdue <gnperdue@me.com>
2018-12-16 09:37:07 -0600

    use common defs for refernces, etc.

---
+++ fnem/tests/common_defs.py
@@ -0,0 +1,4 @@
+LOG_TEMPLATE = 'log_machinewithrule_%d'
+PLT_TEMPLATE = 'plt_machinewithrule_%d'
+REFERNECE_LOG = './reference_files/' + (LOG_TEMPLATE % 1544974435) + '.csv.gz'
+REFERENCE_PLT = './reference_files/' + (PLT_TEMPLATE % 1544974435) + '.pdf'

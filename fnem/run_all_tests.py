643860a7e07f61b07fc4bdaa963a1d0199d4af44
Gabriel Perdue <gnperdue@me.com>
2018-12-16 11:34:40 -0600

    add flags to allow for more granular testing

--- fnem/run_all_tests.py
+++ fnem/run_all_tests.py
@@ -2,8 +2,22 @@
 run all the tests
 '''
 import unittest
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--pattern', default='test', type=str,
+                    help='pattern base name')
+parser.add_argument('--verbosity', default=2, type=int,
+                    help='test verbosity (int)')
+
+
+def main(pattern, verbosity):
+    pattern = pattern + '*.py'
+    suite = unittest.TestLoader().discover('./tests/', pattern=pattern)
+    unittest.TextTestRunner(verbosity=verbosity).run(suite)
 
 
 if __name__ == '__main__':
-    suite = unittest.TestLoader().discover('./tests/', pattern='test*.py')
-    unittest.TextTestRunner(verbosity=2).run(suite)
+    args = parser.parse_args()
+    main(**vars(args))

a61e16c283613f0ee04fac1bed01b96ec922d856
Gabriel Perdue <gnperdue@me.com>
2018-12-16 09:30:25 -0600

    irritating to look at noisy matplotlib warnings, but shouldn't just quiet them in case something else pops up

--- fnem/tests/test_machine.py
+++ fnem/tests/test_machine.py
@@ -16,10 +16,6 @@ from sim.data_model import NoiseModel as Noise
 from sim.engines import SimulationMachine
 from policies.rule_based import SimpleRuleBased
 
-import warnings
-# 'error' to stop on warns, 'ignore' to ignore silly matplotlib noise
-warnings.filterwarnings('ignore')
-
 LOG_TEMPLATE = 'log_machinewithrule_%d'
 PLT_TEMPLATE = 'plt_machinewithrule_%d'
 REFERNECE_LOG = './reference_files/' + (LOG_TEMPLATE % 1544881179) + '.csv.gz'

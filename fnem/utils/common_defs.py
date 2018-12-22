7621544d2bb729831e21456be1146e042fbaeda0
Gabriel Perdue <gnperdue@me.com>
2018-12-22 01:11:01 -0600

    add a common defs file to utils for constants, etc. for the main code

---
+++ fnem/utils/common_defs.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+# data amplitudes
+D1AMP, D2AMP, D3AMP, D4AMP = 10.0, 1.0, 0.5, 0.1
+# data update frequencies / pi
+D1FRQ, D2FRQ, D3FRQ, D4FRQ = 1.0, 0.1, 3.0, 10.0
+DTYPE = np.float32
+# noise amplitudes
+N1AMP, N2AMP, N3AMP, N4AMP = 0.05, 0.04, 0.02, 0.01
+
+DEFAULT_COMMANDS = np.array([-0.5, -0.375, -0.25, -0.125, 0.0,
+                             0.125, 0.25, 0.375, 0.5], dtype=DTYPE)

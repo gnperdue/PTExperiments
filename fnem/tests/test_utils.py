c4e0497532f94065ef72c65fdf8bf8e326862f3c
Gabriel Perdue <gnperdue@me.com>
2018-12-22 01:17:01 -0600

    start a test script for the utils module

---
+++ fnem/tests/test_utils.py
@@ -0,0 +1,37 @@
+'''
+Usage:
+    python test_utils.py -v
+    python test_utils.py
+'''
+import logging
+import unittest
+import utils.util_funcs as utils
+
+
+class TestUtils(unittest.TestCase):
+
+    def test_get_logging_level(self):
+        self.assertEqual(logging.INFO,
+                         utils.get_logging_level('info'))
+        self.assertEqual(logging.INFO,
+                         utils.get_logging_level('INFO'))
+        self.assertEqual(logging.DEBUG,
+                         utils.get_logging_level('debug'))
+        self.assertEqual(logging.DEBUG,
+                         utils.get_logging_level('DEBUG'))
+        self.assertEqual(logging.WARNING,
+                         utils.get_logging_level('warning'))
+        self.assertEqual(logging.WARNING,
+                         utils.get_logging_level('WARNING'))
+        self.assertEqual(logging.ERROR,
+                         utils.get_logging_level('error'))
+        self.assertEqual(logging.ERROR,
+                         utils.get_logging_level('ERROR'))
+        self.assertEqual(logging.CRITICAL,
+                         utils.get_logging_level('critical'))
+        self.assertEqual(logging.CRITICAL,
+                         utils.get_logging_level('CRITICAL'))
+
+
+if __name__ == '__main__':
+    unittest.main()

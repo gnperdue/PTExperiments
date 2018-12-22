f939c56fb2167a2f069ee875715ee71b8aaa0689
Gabriel Perdue <gnperdue@me.com>
2018-12-22 01:18:12 -0600

    add a trainer base class to define the API

--- fnem/trainers/trainers.py
+++ fnem/trainers/trainers.py
@@ -5,14 +5,33 @@ import logging
 LOGGER = logging.getLogger(__name__)
 
 
-class HistoricalTrainer(object):
-    '''data source is a file to loop over'''
+class Trainer(object):
+    '''base class - defines the API'''
 
-    def __init__(self, training_file):
+    def __init__(self, policy):
         self.device = torch.device(
             'cuda:0' if torch.cuda.is_available() else 'cpu'
         )
         LOGGER.info('Device = {}'.format(self.device))
+        self.policy = policy
+        self.training_sim_machine = None
+        self.training_data_file = None
+
+    def build_or_restore_model_and_optimizer(self):
+        raise NotImplementedError
+
+    def train_model_with_target_replay(self):
+        raise NotImplementedError
+
+    def save_performance_plots(self):
+        raise NotImplementedError
+
+
+class HistoricalTrainer(Trainer):
+    '''data source is a file to loop over'''
+
+    def __init__(self, policy, training_file):
+        super(HistoricalTrainer, self).__init__(policy=policy)
         self.training_data_file = training_file
 
     def build_or_restore_model_and_optimizer(self):
@@ -25,6 +44,9 @@ class HistoricalTrainer(object):
         pass
 
 
-class LiveTrainer(object):
-    '''data source is a class we interrogate for steps and values'''
-    pass
+class LiveTrainer(Trainer):
+    '''data source is a sim machine we interrogate for steps and values'''
+
+    def __init__(self, policy, sim_machine):
+        super(LiveTrainer, self).__init__(policy=policy)
+        self.training_sim_machine = sim_machine

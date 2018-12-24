38d4d1727e5e612c181f97d6d67d2f579109465b
Gabriel Perdue <gnperdue@me.com>
2018-12-24 17:36:53 +0530

    add some comments, move `RUN_MODES` to common_defs

--- fnem/utils/common_defs.py
+++ fnem/utils/common_defs.py
@@ -8,5 +8,11 @@ DTYPE = np.float32
 # noise amplitudes
 N1AMP, N2AMP, N3AMP, N4AMP = 0.05, 0.04, 0.02, 0.01
 
+# machine update "dial settings" (if allowing only discreet updates)
 DEFAULT_COMMANDS = np.array([-0.5, -0.375, -0.25, -0.125, 0.0,
                              0.125, 0.25, 0.375, 0.5], dtype=DTYPE)
+
+# strategies for running a trainer class
+RUN_MODES = [
+    'RUN-TRAINED', 'TRAIN-HISTORICAL', 'TRAIN-LIVE'
+]

c9a35ab3114aa9df2d0b0788f09b6be6579330c1
Gabriel Perdue <gnperdue@me.com>
2018-12-25 19:47:54 +0530

    use new policy base class

--- fnem/policies/rule_based.py
+++ fnem/policies/rule_based.py
@@ -1,32 +1,19 @@
 import numpy as np
-from sim.data_model import DTYPE
+from .base import BasePolicy
 
 
-class SimpleRuleBased(object):
-    '''
-    intended operation:
-    1. observe the machine state
-    2. compute a new setting value and apply it
-    '''
+class SimpleRuleBased(BasePolicy):
 
     def __init__(
         self, time, setting, amplitude, period, commands_array
     ):
-        '''
-        amplitude = max value, period = max -> min -> max t
-        '''
-        self._time = time
-        self._setting = setting
-        self._amplitude = amplitude
-        self._period = period
-        self._state = None
-        self._commands = np.asarray(commands_array, dtype=DTYPE)
+        super(SimpleRuleBased, self).__init__(
+            time, setting, amplitude, period, commands_array
+        )
 
     def set_state(self, sensor_array):
         '''
-        sensor_array should be a 5 element array - one for each sensor, t.
         this policy ignores state and only uses t
-
         TODO - this should take a minibatch, not a single point
         '''
         self._state = sensor_array[0:4]
@@ -49,10 +36,6 @@ class SimpleRuleBased(object):
         diffs = np.abs(self._commands - delta)
         command = np.argmin(diffs)
         return command
-
-    def update_setting(self, command):
-        '''command is the index of the step change'''
-        self._setting = self._setting + self._commands[command]
 
     def build_or_restore_model_and_optimizer(self):
         '''function for API compatability only'''

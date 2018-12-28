a79821b9ee53b0abf998e11ee8ac50d566452eee
Gabriel Perdue <gnperdue@me.com>
2018-12-28 10:56:27 +0530

    script for PYTHONPATH manip, not really needed...

---
+++ fnem/setpypath.sh
@@ -0,0 +1,7 @@
+export PYTHONPATH=`pwd`:${PYTHONPATH}
+export PYTHONPATH=`pwd`/policies/:${PYTHONPATH}
+export PYTHONPATH=`pwd`/reference_files/:${PYTHONPATH}
+export PYTHONPATH=`pwd`/sim/:${PYTHONPATH}
+export PYTHONPATH=`pwd`/tests/:${PYTHONPATH}
+export PYTHONPATH=`pwd`/trainers/:${PYTHONPATH}
+export PYTHONPATH=`pwd`/utils/:${PYTHONPATH}

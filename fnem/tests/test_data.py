7ce6e2431b2f9cdf90750151c727ce2b25cc5781
Gabriel Perdue <gnperdue@me.com>
2019-01-01 18:26:32 +0530

    expand dataset tests in fnem

--- fnem/tests/test_data.py
+++ fnem/tests/test_data.py
@@ -4,8 +4,10 @@ Usage:
     python test_data.py
 '''
 import unittest
+import os
 import time
 
+import numpy as np
 import torch
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
@@ -14,6 +16,7 @@ from datasets.historical_data import HistoricalDataset
 from datasets.live_data import LiveDataset
 from datasets.live_data import LiveToTensor
 from utils.common_defs import DATASET_MACHINE_LOG_TEMPLATE
+from utils.common_defs import DATASET_MACHINE_REFERENCE_LOG
 
 
 class TestHistoricalDataset(unittest.TestCase):
@@ -31,6 +34,7 @@ class TestHistoricalDataset(unittest.TestCase):
 class TestLiveDataset(unittest.TestCase):
 
     def setUp(self):
+        np.random.seed(0)
         self.test_time = time.time()
         self.maxsteps = 100
         self.logname = './' + DATASET_MACHINE_LOG_TEMPLATE % self.test_time
@@ -45,17 +49,20 @@ class TestLiveDataset(unittest.TestCase):
         pass
 
     def test_dataset_gen(self):
+        batch_size = 10
         dl = DataLoader(
-            self.dataset, batch_size=10, shuffle=False, num_workers=1
+            self.dataset, batch_size=batch_size, shuffle=False, num_workers=1
         )
         for i, sample in enumerate(dl):
             self.assertEqual(sample.shape, torch.Size([10, 7]))
+        self.assertEqual(i, self.maxsteps / batch_size - 1)
+        # we need to kill some time before closing the logger since the
+        # dataloader is doing its thing on a different thread
+        time.sleep(1)
         self.dataset.close_machine_logger()
-
-        # reference_log_size = os.stat(REFERNECE_LOG).st_size
-        # new_log_size = os.stat(self.machine_log + '.csv.gz').st_size
-        # self.assertEqual(reference_log_size, new_log_size)
-
+        reference_log_size = os.stat(DATASET_MACHINE_REFERENCE_LOG).st_size
+        new_log_size = os.stat(self.logname + '.csv.gz').st_size
+        self.assertEqual(reference_log_size, new_log_size)
 
     def test_dataset_len(self):
         self.assertEqual(self.maxsteps, len(self.dataset))

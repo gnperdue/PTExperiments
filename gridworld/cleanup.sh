ae11698bd5c1930c4bcc3c220d9bcc5b76e61a0c
Gabriel Perdue <gnperdue@me.com>
2019-01-07 12:56:33 +0530

    check existence before moving a file while cleaning up in gridworld

--- gridworld/cleanup.sh
+++ gridworld/cleanup.sh
@@ -5,6 +5,11 @@ rm -f log_nnqrt*.txt
 rm -f deepq_targrep_*pdf
 rm -f test_losses.npy
 rm -f test_winpct.npy
-mv ckpt.tar archive/t${DAT}_ckpt.tar
-mv losses.npy archive/t${DAT}_losses.npy
-mv winpct.npy archive/t${DAT}_winpct.npy
+
+FILELIST="ckpt.tar losses.npy winpct.npy"
+for file in $FILELIST
+do
+  if [[ -e $file ]]; then
+    mv $file archive/t${DAT}_${file}
+  fi
+done

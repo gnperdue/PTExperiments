341ea8efc24d9dcff1541d53bd31b86514f76e14
Gabriel Perdue <gnperdue@me.com>
2019-01-19 09:42:50 -0600

    no need to unpack pytorch tensor and then repack it for setting up state for training in fnem

--- fnem/trainers/trainers.py
+++ fnem/trainers/trainers.py
@@ -68,7 +68,7 @@ class HistoricalTrainer(Trainer):
             heats_buffer = []
             for i, data in enumerate(self.data_source):
                 setting = self.data_source.get_setting()
-                historical_state = data[0]
+                historical_state = data[0] # true sensor vals in data[2]
                 sensor_vals = list(historical_state[0:4].numpy())
                 target_setting = historical_state[4].item()
                 t = historical_state[5].item()
@@ -80,7 +80,7 @@ class HistoricalTrainer(Trainer):
                 self.settings.append(setting)
                 self.setting_diffs.append(target_setting - setting)
                 self.ts.append(t)
-                state = torch.Tensor(np.append(sensor_vals, [setting, t]))
+                state = data[0] # historical_state
                 if len(sequence_buffer) < self.sequence_size:
                     sequence_buffer.append(state)
                     heats_buffer.append(heat)
@@ -155,7 +155,7 @@ class LiveTrainer(Trainer):
             self.heats.append(heat.item())
             self.settings.append(setting)
             self.ts.append(t)
-            state = torch.Tensor(np.append(sensor_vals, [setting, t]))
+            state = data[0]
             if len(sequence_buffer) < self.sequence_size:
                 sequence_buffer.append(state)
                 heats_buffer.append(heat)

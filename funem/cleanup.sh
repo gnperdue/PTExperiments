1b1bc241119f723b6dc87dfc19ea1c3d39e8821e
Gabriel Perdue <gnperdue@me.com>
2019-01-28 06:54:12 -0600

    clean up live train plots

--- funem/cleanup.sh
+++ funem/cleanup.sh
@@ -3,6 +3,7 @@ rm -f log_engine_*.csv*
 rm -f tmplog.csv*
 rm -f log_live_datasource_*.csv*
 rm -f log_master_run*txt
+rm -f plt_live_trainer_*pdf
 DIRS="datasources qlearners sim tests trainers utils"
 for dir in $DIRS
 do

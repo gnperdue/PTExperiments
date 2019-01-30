df5ff1ac9af1d7d2583204bd618699cd93bbe024
Gabriel Perdue <gnperdue@me.com>
2019-01-30 13:28:27 -0600

    add a logger to the python test runner in funem

--- funem/try_all_tests.py
+++ funem/try_all_tests.py
@@ -3,6 +3,8 @@ run all the tests
 '''
 import unittest
 import argparse
+import time
+import logging
 
 
 parser = argparse.ArgumentParser()
@@ -13,6 +15,17 @@ parser.add_argument('--verbosity', default=2, type=int,
 
 
 def main(pattern, verbosity):
+    run_time = int(time.time())
+    logfilename = 'log_' + __file__.split('/')[-1].split('.')[0] \
+        + str(run_time) + '.txt'
+    logging.basicConfig(
+        filename=logfilename, level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    LOGGER = logging.getLogger(__name__)
+    LOGGER.info("Starting...")
+    LOGGER.info(__file__)
+
     pattern = pattern + '*.py'
     suite = unittest.TestLoader().discover('./tests/', pattern=pattern)
     unittest.TextTestRunner(verbosity=verbosity).run(suite)

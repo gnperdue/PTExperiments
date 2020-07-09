20774975048a2c1180f48999760d494f05709a81
Gabriel Perdue <gnperdue@me.com>
2020-07-09 13:34:23 -0500

    add a data visualizer script

---
+++ adversarial_fashion/dset_viz.py
@@ -0,0 +1,45 @@
+'''
+plot star-galaxy images
+'''
+import argparse
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+from ptlib.dataloaders import StarGalaxyDataManager as DataManager
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--batch-size', default=6, type=int, help='batch size')
+parser.add_argument('--data-dir', default='', type=str, help='data dir')
+parser.add_argument('--num-batches', default=1, type=int,
+                    help='number of batches')
+parser.add_argument('--pdf-name', default='evt_all.pdf', type=str,
+                    help='output pdf name')
+
+
+def main(batch_size, data_dir, num_batches, pdf_name):
+    data_manager = DataManager(data_dir=data_dir)
+    data_manager.make_means()
+    train_dl, _, _ = data_manager.get_data_loaders(
+        batch_size=batch_size, standardize=False)
+    with PdfPages(pdf_name) as pdf:
+        for iter_num, (inputs, labels) in enumerate(train_dl, 0):
+            if iter_num >= num_batches:
+                break
+            n_cols = int(np.sqrt(batch_size))
+            n_rows = int(np.ceil(batch_size / n_cols))
+            # make grid, plots
+            gs = plt.GridSpec(n_rows, n_cols)
+            for evt, img_tnsr in enumerate(inputs):
+                ax = plt.subplot(gs[evt])
+                ax.axis('on')
+                ax.xaxis.set_major_locator(plt.NullLocator())
+                ax.yaxis.set_major_locator(plt.NullLocator())
+                _ = ax.imshow(
+                    np.moveaxis(img_tnsr.numpy().astype('uint8'), 0, -1))
+                plt.title(data_manager.label_names[labels[evt]])
+            pdf.savefig()
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(**vars(args))
---
+++ adversarial_fashion/run_dset_viz.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+EXE="dset_viz.py"
+ARGS="--batch-size 6"
+ARGS+=" --data-dir /Users/perdue/Dropbox/Data/StarGalaxy"
+ARGS+=" --num-batches 3"
+
+# show exe and args
+cat << EOF
+python $EXE $ARGS
+EOF
+
+python $EXE $ARGS
+
+echo -e "\a"

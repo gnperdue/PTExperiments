1b84da4d1de3261324577b839495b8a0bce29f04
Gabriel Perdue <gnperdue@me.com>
2020-07-10 06:37:06 -0500

    begin to flesh out 'train_attack' (not sure about method name)

--- adversarial_classifier/ptlib/attackers.py
+++ adversarial_classifier/ptlib/attackers.py
@@ -23,7 +23,7 @@ class FGSMAttacker(ModelHandlerBase):
             LOGGER.warning(e)
             self.epsilons = [epsilons]
 
-    def fgsm_attack(image, epsilon, data_grad):
+    def fgsm_attack(self, image, epsilon, data_grad):
         '''
         perturbed image = image + epsilon * sign(data_grad)
         '''
@@ -32,5 +32,30 @@ class FGSMAttacker(ModelHandlerBase):
         perturbed_image = torch.clamp(perturbed_image, 0, 1)
         return perturbed_image
 
-    def attack_for_single_epsilon(self, epsilon):
-        pass
+    # TODO - add open and close HDF5 file methods for logging adv images
+
+    def train_attack_for_single_epsilon(self, epsilon, short_test=False):
+        LOGGER.info(
+            'train_attack_for_single_epsilon for eps = {}'.format(epsilon))
+        train_dl, _, _ = self.dm.get_data_loaders(batch_size=1)
+        correct = 0
+        adv_examples = []
+
+        for iter_num, (inputs, labels) in enumerate(train_dl, 0):
+            if short_test and iter_num >= 20:
+                break
+            inputs.requires_grad = True
+            output = self.model(inputs)
+            initial_pred = output.max(1, keepdim=True)[1][0]
+            LOGGER.debug('initial pred = {}, label = {}'.format(
+                initial_pred.item(), labels.item()))
+            # TODO - not sure about this, want to attack wrong labels also?
+            # if initial_pred.item() != labels.item():
+            #     continue
+            loss = self.criterion(output, labels)
+            LOGGER.debug('loss = {}'.format(loss.item()))
+            self.model.zero_grad()
+            loss.backward()
+            inputs_grad = inputs.grad.data
+            perturbed_inputs = self.fgsm_attack(inputs, epsilon, inputs_grad)
+            LOGGER.debug('perturbed shape = {}'.format(perturbed_inputs.shape))

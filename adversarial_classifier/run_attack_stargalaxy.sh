5adb009bc21089f67271e5beffc1421ce4e77060
Gabriel Perdue <gnperdue@me.com>
2020-07-14 06:04:21 -0500

    incude git hash in arg

--- adversarial_classifier/run_attack_stargalaxy.sh
+++ adversarial_classifier/run_attack_stargalaxy.sh
@@ -6,8 +6,10 @@ DAT=`date +%s`
 
 ARGS="--ckpt-path sg_ckpt.tar"
 ARGS+=" --data-dir ${DATADIR}"
+ARGS+=" --git-hash `git describe --abbrev=12 --dirty --always`"
 ARGS+=" --log-freq 5"
 ARGS+=" --log-level DEBUG"
+# ARGS+=" --log-level INFO"
 ARGS+=" --short-test"
 
 # show exe and args

c97b2de1102c998c4244044355187f75f386ee85
Gabriel Perdue <gnperdue@me.com>
2020-07-16 10:41:37 -0500

    check different epsilons, new file names

--- adversarial_classifier/run_dset_viz_stargalaxy.sh
+++ adversarial_classifier/run_dset_viz_stargalaxy.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
 DATADIR="/Users/perdue/Dropbox/Data/Workspace"
+FGSMSTR="0_000"
+FGSMSTR="0_010"
+FGSMSTR="0_050"
 
 EXE="dset_viz_stargalaxy.py"
 ARGS="--batch-size 5"
 ARGS+=" --data-dir ${DATADIR}"
-# ARGS+=" --file-override ./fgsm.hdf5"
+ARGS+=" --file-override ./fgsm_${FGSMSTR}.hdf5"
 ARGS+=" --num-batches 8"
+ARGS+=" --pdf-name evts_${FGSMSTR}.pdf"
 
 # show exe and args
 cat << EOF
